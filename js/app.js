7080358d6132c030200dd3a2fc0b933acebb016c
Kova 2.0 <pelakova3@gmail.com>
2020-01-26 20:07:03 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+	window.onload = function() { 
+
+    	data = window.location.hash.replace("#/","").replace(".html","");
+    	
+    	if(!data) location.href='#/accueil';
+    	
+    	if(data != "accueil") loadview(data);
+
+    }; 
+    
+    $(window).on('hashchange', function() {
+    	
+		data = window.location.hash.replace("#/","").replace(".html","").replace("#","");
+
+		if(data != "accueil") loadview(data);
+		else showproject();
+		
+    });
+    
+    function hideproject() {
+    	
+    	$( "#kova-project" ).slideUp("slow");
+
+    }
+    
+    function showproject() {
+    	
+    	document.title = "Accueil - KovaSK";
+    	$( "#kova-project" ).slideDown();
+    	document.getElementById( 'tab1' ).click();
+
+    }
+    
+    function loadview(data) {
+    	
+        var include = $('#' + data);
+        
+        if (include.html().length == 0) {
+          var file = 'views/' + data + '.html';
+          include.load(file);
+        }
+        
+        document.title = include.attr("titre") + " - KovaSK";
+        
+        hideproject();
+        document.getElementById( 'tab-' + data ).click();
+        
+    }
